aafd83f8b6e6d16d95f5c7c91c6cc9fdef4b5bd0
snehasingh-25 <sneha251104@gmail.com>
2025-08-13 02:19:10 +0530

    added api

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api";
+const API_URL = "https://notesapp-rr5o.onrender.com/api";
 
 export default function useAuth() {
   const [token, setToken] = useState("");
